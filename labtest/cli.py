6b7f5a0559f19c3d5af6005a50fdd01398144de7
Corey Oordt <coreyoordt@gmail.com>
2018-05-03 07:09:29 -0500

    Add dotenv support and configuration

--- labtest/cli.py
+++ labtest/cli.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
 
 import click
+from config import get_config, check_config
+from dotenv import load_dotenv, find_dotenv
+import instance
 
 
-@click.command()
-def main(args=None):
+@click.group(invoke_without_command=True)
+@click.option('--config', '-c', type=click.Path(exists=True), help='Alternate configuration file.')
+@click.option('--verbose', '-v', is_flag=True, default=False, help='Show verbose output.')
+@click.pass_context
+def cli(ctx, config, **kwargs):
     """Console script for labtest"""
-    click.echo("Replace this message by putting your code into "
-               "labtest.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    cfg = get_config(config, **kwargs)
+    if not cfg.validate():
+        click.ClickException(cfg.validation_message())
+    ctx.obj = cfg
 
+cli.add_command(instance.create)
+cli.add_command(check_config, 'check-config')
 
 if __name__ == "__main__":
-    main()
+    load_dotenv(find_dotenv())
+    cli(obj={}, auto_envvar_prefix='LABTEST')
