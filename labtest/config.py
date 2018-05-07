f0105d22d7bc7aacf3f4204c507fc2be34f65425
Corey Oordt <coreyoordt@gmail.com>
2018-05-06 22:55:17 -0500

    Fixing another python 3 thing for Click

--- labtest/config.py
+++ labtest/config.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from future.utils import iteritems
 from past.builtins import basestring
 from .configobj import Config
