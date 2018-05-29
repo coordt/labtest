e20099de35fd86413c44648e70133d862fadb3e7
Corey Oordt <coreyoordt@gmail.com>
2018-05-29 07:33:36 -0500

    Version updated from 0.3.5 to 0.4.0

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ test_requirements = [
 
 setup(
     name='labtest',
-    version='0.3.5',
+    version='0.4.0',
     description="Build an isolated test lab for running software in containers.",
     long_description=readme + '\n\n' + history,
     author="Corey Oordt",
