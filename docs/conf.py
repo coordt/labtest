7484f398f5e13f5560e35da91bb562f7c9557213
Corey Oordt <coreyoordt@gmail.com>
2018-06-01 07:54:22 -0500

    [UPDATE] Adding links to home page and fixed latex and other rendering

--- docs/conf.py
+++ docs/conf.py
@@ -228,7 +228,7 @@ latex_elements = {
 # (source start file, target name, title, author, documentclass
 # [howto/manual]).
 latex_documents = [
-    ('index', 'labtest.tex',
+    ('contents', 'labtest.tex',
      u'Lab Test Documentation',
      u'Corey Oordt', 'manual'),
 ]
@@ -259,7 +259,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'labtest',
+    ('contents', 'labtest',
      u'Lab Test Documentation',
      [u'Corey Oordt'], 1)
 ]
@@ -274,7 +274,7 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'labtest',
+    ('contents', 'labtest',
      u'Lab Test Documentation',
      u'Corey Oordt',
      'labtest',
